f08b3e00ff4cdb7eee73a1a4b4f5865750687122
Md Israfil <mdronnieronniers@gmail.com>
2025-09-13 23:00:59 +0600

    Add Social login links field and investorLogin field in setting model

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,68 +1,74 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type {
-  BaseQueryFn,
-  FetchArgs,
-  FetchBaseQueryError,
+	BaseQueryFn,
+	FetchArgs,
+	FetchBaseQueryError,
 } from "@reduxjs/toolkit/query";
 import { RootState } from "../store";
 import { logout, setAccessToken } from "../features/auth";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://pamelam.code-commando.com/api",
-  baseUrl: "http://localhost:3000/api",
-  // baseUrl: "https://arcroofs.com/api/v1",
-  // baseUrl: "http://localhost:3000/api",
- prepareHeaders: (headers, { getState }) => {
-  const token = (getState() as RootState).auth?.accessToken;
-  console.log(token, "token");
-  if (token) {
-    headers.set("Authorization", `Bearer ${token}`);
-  }
-  return headers;
-},
+	baseUrl: "/api",
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).auth?.accessToken;
+		console.log(token, "token");
+		if (token) {
+			headers.set("Authorization", `Bearer ${token}`);
+		}
+		return headers;
+	},
 
-  credentials: "include", // ✅ includes HttpOnly cookies like refresh token
+	credentials: "include", // ✅ includes HttpOnly cookies like refresh token
 });
 
 const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
+	string | FetchArgs,
+	unknown,
+	FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions);
+	let result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === 403) {
-    // Refresh access token
-    const refreshResult = await baseQuery(
-      {
-        url: "/auth/refresh-token",
-        method: "POST",
-      },
-      api,
-      extraOptions
-    );
+	if (result.error && result.error.status === 403) {
+		// Refresh access token
+		const refreshResult = await baseQuery(
+			{
+				url: "/auth/refresh-token",
+				method: "POST",
+			},
+			api,
+			extraOptions
+		);
 
-    if (refreshResult.data) {
-      const newAccessToken = (refreshResult.data as any).accessToken;
+		if (refreshResult.data) {
+			const newAccessToken = (refreshResult.data as any).accessToken;
 
-      api.dispatch(setAccessToken(newAccessToken));
+			api.dispatch(setAccessToken(newAccessToken));
 
-      // Retry the original request
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logout());
-    }
-  }
+			// Retry the original request
+			result = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(logout());
+		}
+	}
 
-  return result;
+	return result;
 };
 
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: baseQueryWithReauth,
-  tagTypes: ["auth", "roofing", "window","Product","Pricing","Rules","Faq","Privacy"],
-  endpoints: () => ({}),
+	reducerPath: "baseApi",
+	baseQuery: baseQueryWithReauth,
+	tagTypes: [
+		"auth",
+		"roofing",
+		"window",
+		"Product",
+		"Pricing",
+		"Rules",
+		"Faq",
+		"Privacy",
+	],
+	endpoints: () => ({}),
 });
 
 export default baseApi;
